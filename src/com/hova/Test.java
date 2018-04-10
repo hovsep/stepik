354f8e65d51f1550ef37963789b6ac09a81af737
hovsep <avakyan.hovsep@yandex.ru>
2018-04-10 23:47:43 +0300

    Exceptions

--- src/com/hova/Test.java
+++ src/com/hova/Test.java
@@ -1,22 +1,35 @@
 package com.hova;
 
-import javax.xml.soap.Text;
 
 public class Test {
 
     public static void main(String[] args) {
-
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
     }
 
-    public Label checkLabels(TextAnalyzer[] analyzers, String text) {
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
+    }
 
-        for(TextAnalyzer ta : analyzers) {
-            Label l = ta.processText(text);
-            if (Label.OK != l) {
-                return l;
+    public static String getCallerClassAndMethodName() {
+        String caller = "";
+
+        try {
+            throw new Exception("Dummy");
+
+        } catch (Exception e) {
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            if (stackTrace.length < 3) {
+                caller = null;
+            } else {
+                StackTraceElement ste = stackTrace[2];
+                caller = ste.getClassName() + "#" + ste.getMethodName();
             }
+
         }
 
-        return Label.OK;
+        return caller;
     }
+
 }
