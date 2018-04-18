94faa23e8786b4a63e71eb35312359a5ce4d4d9c
hovsep <avakyan.hovsep@yandex.ru>
2018-04-18 23:02:56 +0300

    Checksum task

--- src/com/hova/Test.java
+++ src/com/hova/Test.java
@@ -1,15 +1,39 @@
 package com.hova;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Test {
-
-    public static final String AUSTIN_POWERS = "Austin Powers";
-    public static final String WEAPONS = "weapons";
-    public static final String BANNED_SUBSTANCE = "banned substance";
-
     public static void main(String[] args) {
+        InputStream test = new ByteArrayInputStream(new byte[] {0x33, 0x45, 0x01});
+
+        try {
+            System.out.println(checkSumOfStream(test));
+        } catch (Exception e) {
+            System.out.println("AAAAArh!" + e.getMessage());
+        }
 
     }
 
+
+    public static int checkSumOfStream(InputStream inputStream) throws IOException {
+        try {
+            int p = 0,n;
+
+            while ((n = inputStream.read()) != -1) {
+               if (0 == p) {
+                   p = Integer.rotateLeft(0,1)^n;
+               } else {
+                   p = Integer.rotateLeft(p,1)^n;
+               }
+            }
+
+            return p;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 
 }
