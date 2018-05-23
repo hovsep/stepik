32fea1b86fee9644422866fe1b1a2e050743d112
Hovsep <avakyan.hovsep@yandex.ru>
2018-05-23 17:19:52 +0300

    Utf to bytes

--- src/com/hova/Test.java
+++ src/com/hova/Test.java
@@ -1,54 +1,17 @@
 package com.hova;
 
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class Test {
 
     public static void main(String[] args) {
-        test();
-    }
-
-    public static void test() {
-        ByteArrayInputStream ti = new ByteArrayInputStream(new byte[] {65,13, 10, 10, 13});
-        try {
-            int b = 0 ;
-            boolean f = false;
-
-            while ((b = ti.read()) != -1) {
-
-                if (f) {
-                    if (b == 10) {
-                        System.out.println(10);
-                        f = false;
-                        continue;
-                    } else {
-                        System.out.println(13);
-                    }
-
-                }
-
-
-
-                if (b == 13) {
-                    f = true;
-                } else {
-                    System.out.println(b);
-                    f = false;
-                }
-
-            }
-
-            if (f) {
-                System.out.println(13);
-            }
-
-            System.out.flush();
-
-        } catch (Exception e) {
-
+        String s = "Ы";
+        byte[] raw = s.getBytes(StandardCharsets.UTF_8);
+        for (byte b:raw) {
+            System.out.println(Byte.toUnsignedInt(b));
         }
     }
+
 
 }
