5fd4429216f71d69071c1c177a7977af8f530cfe
Hovsep <avakyan.hovsep@yandex.ru>
2018-06-13 16:13:56 +0300

    Object serialization\deserialization

--- src/com/hova/Test.java
+++ src/com/hova/Test.java
@@ -1,32 +1,56 @@
 package com.hova;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+import java.io.*;
+import java.util.Scanner;
 
 public class Test {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        Animal[] animalM1 = {
+                new Animal("Cat"),
+                new Animal("Dog"),
+                new Animal("Elephant"),
+                new Animal("Cock"),
+                new Animal("Bull"),
+                new Animal("Ant"),
+                new Animal("Tentecles"),
+                new Animal("Worm")};
+        ByteArrayOutputStream bai = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bai);
+        oos.writeInt(animalM1.length);
+        for (int i = 0; i < animalM1.length; i++) {
+            oos.writeObject(animalM1[i]);
+            oos.writeInt(333);
+        }
+        oos.flush();
+        oos.close();
+
+        deserializeAnimalArray(bai.toByteArray());
+    }
+
+
+    public static Animal[] deserializeAnimalArray(byte[] data) {
         try {
-            System.out.print(readAsString(System.in, StandardCharsets.UTF_8));
+            Animal a;
+
+            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+            int c = ois.readInt();
+            Animal[] res = new Animal[c];
+
+            for (int i = 0; i<c; i++) {
+                a = (Animal) ois.readObject();
+                res[i] = a;
+            }
+
+            return res;
         } catch (Exception e) {
-
+            throw new IllegalArgumentException();
         }
 
     }
 
-    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        InputStreamReader reader = new InputStreamReader(inputStream, charset);
-        StringBuilder sb = new StringBuilder();
-        int b;
-        while ((b = reader.read()) != -1) {
-            sb.append(Character.toChars(b));
-        }
-
-        return sb.toString();
-    }
 
 
 }
