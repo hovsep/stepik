28ebf6058c419a30ed8b3bf6effdddea2b187fb3
Hovsep <avakyan.hovsep@yandex.ru>
2018-06-15 18:01:22 +0300

    HashSet

--- src/com/hova/Test.java
+++ src/com/hova/Test.java
@@ -1,22 +1,50 @@
 package com.hova;
 
 
+import java.util.*;
+
 public class Test {
 
 
     public static void main(String[] args) {
-        Pair<Integer, String> pair = Pair.of(1, "hello");
-        Integer i = pair.getFirst(); // 1
-        System.out.println(i);
-        String s = pair.getSecond(); // "hello"
-        System.out.println(s);
+       HashSet<Integer> s1 = new HashSet<>();
+       HashSet<Integer> s2 = new HashSet<>();
+       Set d;
 
-        Pair<Integer, String> pair2 = Pair.of(1, "hello");
-        boolean mustBeTrue = pair.equals(pair2); // true!
-        System.out.println(mustBeTrue);
-        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-        System.out.println(mustAlsoBeTrue);
+       s1.add(1);
+       s1.add(2);
+       s1.add(3);
+
+       s2.add(0);
+       s2.add(1);
+       s2.add(2);
+
+       d = symmetricDifference(s1,s2);
+
+       Iterator<Integer> i = d.iterator();
+
+       while(i.hasNext()) {
+           System.out.println(i.next());
+       }
     }
+
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+        HashSet<Integer> d,s1,s2;
+
+        s1 = new HashSet(set1);
+        s2 = new HashSet(set2);
+        d = new HashSet<>();
+
+        s1.removeAll(set2);
+        s2.removeAll(set1);
+
+        d.addAll(s1);
+        d.addAll(s2);
+
+        return (Set) d;
+    }
+
+
 
 
 
