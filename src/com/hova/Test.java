119ed6a3fabb436b71ac8358c2c3bbe36e412e40
hovsep <avakyan.hovsep@yandex.ru>
2018-06-21 02:55:22 +0300

    Stream API

--- src/com/hova/Test.java
+++ src/com/hova/Test.java
@@ -1,28 +1,25 @@
 package com.hova;
 
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class Test {
 
 
     public static void main(String[] args) {
-        Predicate<Object> condition = Objects::isNull;
-        Function<Object, Integer> ifTrue = obj -> 0;
-        Function<CharSequence, Integer> ifFalse = CharSequence::length;
-        Function<String, Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+        IntStream s = pseudoRandomStream(13);
+
+        s.forEach(System.out::println);
     }
 
-    public static <T, U> Function<T, U> ternaryOperator(
-            Predicate<? super T> condition,
-            Function<? super T, ? extends U> ifTrue,
-            Function<? super T, ? extends U> ifFalse) {
+    public static IntStream pseudoRandomStream(int seed) {
+        IntStream s = IntStream.iterate(seed, n -> {
+            Integer nsq = n * n;
+            return nsq % 10000 / 10;
+        }).limit(seed);
 
-        return x -> {
-            return condition.test(x) ? ifTrue.apply(x) : ifFalse.apply(x);
-        };
-
+        return s;
     }
+
+
 
 }
